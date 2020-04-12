d2992521b9b9fe4622cc8a33b56af5d6d529f35e
Don Kirkby <donkirkby@gmail.com>
2020-04-11 21:42:43 -0700

    Show all other questions in background.

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # top-stack
 Query top posts from Stack Overflow
 
-Download data from [top posts] by user.
+Download data from [my top posts]. Compare to general [technology trends] for
+Spring and Node.
 
 Example plot:
 
 ![score over time]
 
-[top posts]: https://data.stackexchange.com/stackoverflow/query/1224025/top-post-history-by-user?UserId=4794
+[my top posts]: https://data.stackexchange.com/stackoverflow/query/1224025/top-post-history-by-user?UserId=4794
 [score over time]: top11.png
+[technology trends]: https://insights.stackoverflow.com/trends?tags=npm%2Cspring%2Cnode.js
--- top_stack.py
+++ top_stack.py
@@ -9,13 +9,14 @@ import matplotlib.pyplot as plt
 def main():
     with open('post_votes.csv') as votes:
         rows = list(DictReader(votes))
+    if __name__ == '__live_coding__':
+        rows = rows[:1000]
 
     today = datetime.today()
+    highlight_count = 0
 
-    for (post_type, title), post_rows in groupby(rows,
-                                                 itemgetter('postType',
-                                                            'title')):
-        summary = f'{post_type[0]}: {title[:35]}'
+    for post_id, post_rows in groupby(rows, itemgetter('postId')):
+        post_rows = list(post_rows)
         dates = []
         scores = []
         score = 0
@@ -23,7 +24,11 @@ def main():
             event = row['event']
             if event in ('AcceptedByOriginator',
                          'Favorite',
-                         'ApproveEditSuggestion'):
+                         'ApproveEditSuggestion',
+                         'BountyStart',
+                         'BountyClose',
+                         'ModeratorReview',
+                         'Undeletion'):
                 continue
             if event == 'UpMod':
                 score += 1
@@ -33,11 +38,22 @@ def main():
             dates.append(datetime.strptime(row['creationDate'],
                                            "%Y-%m-%d %H:%M:%S"))
             scores.append(score)
+        row = post_rows[0]
+        if row['currentRank'] in {'1', '2', '3'} or row['postId'] == '46216':
+            summary = f'{row["postType"][0]}: {row["title"][:45]}'
+            highlight_count += 1
+            color = f'C{highlight_count}'
+            alpha = None
+        else:
+            summary = None
+            color = 'k'
+            alpha = 0.07
         dates.append(today)
         scores.append(score)
-        plt.plot(dates, scores, label=summary)
+
+        plt.plot(dates, scores, label=summary, color=color, alpha=alpha)
     plt.legend()
-    plt.title('Top posts')
+    plt.title('My Top Posts')
     plt.ylabel('Score')
     plt.xlabel('Date')
     plt.tight_layout()
