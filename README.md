e5a8956f88314b13021f128c6eafe38cf8a55d2c
Don Kirkby <donkirkby@gmail.com>
2020-04-12 09:01:46 -0700

    Reduce splines to reduce plot's clutter.
    Also include zero score on creation date for each post.

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ Example plot:
 
 ![score over time]
 
+Consider running query for all questions in a [tag].
+
 [my top posts]: https://data.stackexchange.com/stackoverflow/query/1224025/top-post-history-by-user?UserId=4794
 [score over time]: top11.png
 [technology trends]: https://insights.stackoverflow.com/trends?tags=npm%2Cspring%2Cnode.js
+[tag]: https://data.stackexchange.com/stackoverflow/query/1224433/top-post-history-by-tag?tag=python
--- top_stack.py
+++ top_stack.py
@@ -4,22 +4,30 @@ from itertools import groupby
 from operator import itemgetter
 
 import matplotlib.pyplot as plt
+from matplotlib.dates import date2num
 
 
 def main():
     with open('post_votes.csv') as votes:
         rows = list(DictReader(votes))
     if __name__ == '__live_coding__':
-        rows = rows[:1000]
+        rows = rows[:10000:20]
 
     today = datetime.today()
     highlight_count = 0
+    max_score = 0
+    min_score = 0
+    start_date = None
 
     for post_id, post_rows in groupby(rows, itemgetter('postId')):
         post_rows = list(post_rows)
-        dates = []
-        scores = []
+        first_row = post_rows[0]
         score = 0
+        creation_date = parse_date(first_row, 'creationDate')
+        if start_date is None or creation_date < start_date:
+            start_date = creation_date
+        dates = [creation_date]
+        scores = [score]
         for row in post_rows:
             event = row['event']
             if event in ('AcceptedByOriginator',
@@ -35,10 +43,12 @@ def main():
             else:
                 assert event == 'DownMod', event
                 score -= 1
-            dates.append(datetime.strptime(row['creationDate'],
-                                           "%Y-%m-%d %H:%M:%S"))
+            max_score = max(score, max_score)
+            min_score = min(score, min_score)
+            dates.append(parse_date(row, 'voteDate'))
             scores.append(score)
-        row = post_rows[0]
+
+        row = first_row
         if row['currentRank'] in {'1', '2', '3'} or row['postId'] == '46216':
             summary = f'{row["postType"][0]}: {row["title"][:45]}'
             highlight_count += 1
@@ -52,12 +62,24 @@ def main():
         scores.append(score)
 
         plt.plot(dates, scores, label=summary, color=color, alpha=alpha)
+
+    axes = plt.gca()
+    axes.spines['top'].set_visible(False)
+    axes.spines['right'].set_visible(False)
+    axes.spines['left'].set_bounds(min_score, max_score)
+    axes.spines['bottom'].set_bounds(date2num(start_date), date2num(today))
+
     plt.legend()
     plt.title('My Top Posts')
     plt.ylabel('Score')
     plt.xlabel('Date')
     plt.tight_layout()
+
     plt.show()
+
+
+def parse_date(row, field_name):
+    return datetime.strptime(row[field_name], "%Y-%m-%d %H:%M:%S")
 
 
 main()
